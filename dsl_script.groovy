4fbfe4f94bd5b034b1aa0eb767bb55ccb2ecae55
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-22 16:50:10 +0300

    Child job DSL code created

--- dsl_script.groovy
+++ dsl_script.groovy
@@ -1,42 +1,48 @@
-def branchNane = "ilakhtenkov"
+def branchName = "ilakhtenkov"
 def folderName = "Igor Lakhtenkov"
-job(folderName+'/MNTLAB-'+branchNane+'-main-build-job') {
-description 'This is test main job'
-parameters {
-activeChoiceParam('BRANCH_NAME') {
-    description('Allows branch multiple choices')
-    filterable()
-    choiceType('SINGLE_SELECT')
-    groovyScript {
-	script('''def getbranches = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute()
-				return getbranches.text.readLines()
-				.collect { it.split()[1].replaceAll('refs/heads/', '')  }
-				.unique()
-				.findAll { it.matches('ilakhtenkov|master') }''')
-	fallbackScript('return ["error"]')
-    }
-}
-}
-parameters {
-activeChoiceParam('BUILDS_TRIGGER') {
-    description('Allows triggered child jobs multiple choices')
-    filterable()
-    choiceType('CHECKBOX')
-    groovyScript {
-	script('''import jenkins.model.*;
-					  import hudson.model.*
+def childJobsNumber = 4
 
-					  def list =[]
-					  Jenkins.instance.getAllItems(AbstractProject.class).each {it ->
-						if (it.fullName.matches('Igor Lakhtenkov\\/MNTLAB-ilakhtenkov-child(.+)')) {
-						list << "${it.name}:selected"
-						}
-					  }
-					  return list''')
-	fallbackScript('return ["error"]')
+folder(folderName) {
+    description('This is folder of'+folderName)
+}
+
+job(folderName+'/MNTLAB-'+branchName+'-main-build-job') {
+    description 'This is test main job'
+    parameters {
+        activeChoiceParam('BRANCH_NAME') {
+            description('Allows branch single choice')
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('''def getbranches = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute()
+					return getbranches.text.readLines()
+        				.collect { it.split()[1].replaceAll('refs/heads/', '')  }
+        				.unique()
+        				.findAll { it.matches('ilakhtenkov|master') }''')
+                fallbackScript('return ["error"]')
+            }
+        }
     }
-}
-}
+     parameters {
+        activeChoiceParam('BUILDS_TRIGGER') {
+            description('Allows triggered child jobs multiple choices')
+            filterable()
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('''import jenkins.model.*;
+						  import hudson.model.*
+
+						  def list =[]
+						  Jenkins.instance.getAllItems(AbstractProject.class).each {it ->
+  							if (it.fullName.matches('Igor Lakhtenkov\\/MNTLAB-ilakhtenkov-child(.+)')) {
+    							list << "${it.name}:selected"
+  							}
+						  }
+						  return list''')
+                fallbackScript('return ["error"]')
+            }
+        }
+     }
     scm {
         git {
           remote {
@@ -69,4 +75,35 @@ activeChoiceParam('BUILDS_TRIGGER') {
         }
     }
 }
-       
+for (i = 0; i < childJobsNumber; i++) {
+    job(folderName+'/MNTLAB-'+branchName+'-child'+i+'-build-job') {
+        description 'This is test main job'
+        parameters {
+            activeChoiceParam('BRANCH_NAME') {
+                description('Allows branch single choice')
+                filterable()
+                choiceType('SINGLE_SELECT')
+                groovyScript {
+                    script('''def getbranches = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute()
+                        return getbranches.text.readLines()
+                            .collect { it.split()[1].replaceAll('refs/heads/', '')  }
+                            .unique() }''')
+                    fallbackScript('return ["error"]')
+                }
+            }
+        }
+        scm {
+            git {
+              remote {
+                url('https://github.com/MNT-Lab/mntlab-dsl.git')
+              }
+              branch('$BRANCH_NAME')
+            }
+        }
+        steps {
+            
+            }
+        }
+    }   
+}
+
