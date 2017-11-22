91e3b38b678b84e320284f0fcc131f7a7d2d81be
Ihar Vauchok <napa93@mail.ru>
2017-11-22 13:36:01 +0000

    Feature/Created folder and jobs

--- jobs.groovy
+++ jobs.groovy
@@ -1,1 +1,18 @@
-#it would be some groovy script
+def student = 'ivauchok'
+def giturl = 'https://github.com/MNT-Lab/mntlab-dsl.git'
+
+folder(student) {
+    description("Folder containing all jobs for ${student}")
+}
+
+job("${student}/MNTLAB-${student}-main-build-job") {
+}
+
+for(i in 1..4) {
+    job("${student}/MNTLAB-${student}-child${i}-build-job") {
+        scm {
+            git(giturl)
+        }
+    }
+}
+
