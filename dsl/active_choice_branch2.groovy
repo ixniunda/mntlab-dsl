1b650d17cdc757348861662caf97ac262aaafabf
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-22 20:11:28 +0300

    Edited active_choice_branch2

--- dsl/active_choice_branch2.groovy
+++ dsl/active_choice_branch2.groovy
@@ -1,17 +1,6 @@
 def getbranches = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute()
+return getbranches.text.readLines()
 
-def list = getbranches.text.readLines()
-        .collect { it.split()[1].replaceAll('refs/heads/', '')  }
-        .unique()
-        .findAll { it.matches('ilakhtenkov|master') }
-def listSelected =[]
-for(int i = 0; i < list.size(); i++)
-{
-    if(list.get(i).contains('ilakhtenkov'))
-    {
-        listSelected.add(list.get(i).replace('ilakhtenkov', 'ilakhtenkov:selected'));
-    } else {
-        listSelected.add(list.get(i));
-    }
-}
-return listSelected
+return getbranches.text.readLines()
+         .collect { it.split()[1].replaceAll('refs/heads/', '')  }
+         .unique()
