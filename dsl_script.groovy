2377104e2cdecbfaed5e026030d19750f73b3969
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-22 18:05:08 +0300

    Fix script

--- dsl_script.groovy
+++ dsl_script.groovy
@@ -35,7 +35,7 @@ import hudson.model.*
 
 def list =[]
 Jenkins.instance.getAllItems(AbstractProject.class).each {it ->
-  if (it.fullName.matches('Igor Lakhtenkov\\\/MNTLAB-ilakhtenkov-child(.+)')) {
+  if (it.fullName.matches('Igor Lakhtenkov\\\\/MNTLAB-ilakhtenkov-child(.+)')) {
     list << "${it.name}:selected"
   }
 }
