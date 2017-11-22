ccee7d50b78df3f4ab5c832e0c6f5e7ba9cd2c84
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-22 20:14:18 +0300

    Edited active_choice_branch2

--- dsl/active_choice_branch2.groovy
+++ dsl/active_choice_branch2.groovy
@@ -1,5 +1,4 @@
 def getbranches = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute()
-return getbranches.text.readLines()
 
 return getbranches.text.readLines()
          .collect { it.split()[1].replaceAll('refs/heads/', '')  }
