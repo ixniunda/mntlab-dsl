5cc6c3c4fe50f61c5dc09ab7e19371da43f101d5
Maksim Bialitski <violentpain@gmail.com>
2017-11-22 14:40:37 +0300

    dsl groovy

--- groovy/dsl.groovy
+++ groovy/dsl.groovy
@@ -11,18 +11,17 @@ job("${job_folder}${job_prefix}-main-${job_suffix}") {
     parameters {
         activeChoiceReactiveParam('BRANCHE_NAME') {
             description('Allows user choose from multiple choices')
-            filterable()
+
             choiceType('SINGLE_SELECT')
             groovyScript {
-                script("return [${git_brunch},'master']")
+                script("return ['${git_brunch}','master']")
             }
         }
         activeChoiceReactiveParam ('Job') {
             description('Select job to build')
-            filterable()
             choiceType('CHECKBOX')
             groovyScript{
-                script ("list=[];(1..4).each {list.add(\"${job_prefix}-slave${it}-${job_suffix}\")} ")
+                script ("list=[];(1..4).each {list.add(\"${job_prefix}-slave${it}-${job_suffix}\")}; return list ")
                 fallbackScript('')
             }
         }
