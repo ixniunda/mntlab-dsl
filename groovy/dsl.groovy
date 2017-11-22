9d0ecb403b49e81def38b1bc56dbd81ee7410a80
Maksim Bialitski <violentpain@gmail.com>
2017-11-22 14:17:56 +0300

    dsl groovy

---
+++ groovy/dsl.groovy
@@ -0,0 +1,31 @@
+git_url="https://github.com/MNT-Lab/mntlab-dsl.git"
+job_folder="Maksim Bialitski/"
+job_prefix="MNTLAB-MBialitski"
+job_suffix="build-job"
+git_brunch="mbialitski"
+job("${job_folder}-${job_prefix}-main-${job_suffix}") {
+    description 'This is the main build job'
+    scm	{
+        github (git_url,git_brunch)
+    }
+    parameters {
+        activeChoiceReactiveParam('BRANCHE_NAME') {
+            description('Allows user choose from multiple choices')
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script("return [git_brunch,'master']")
+                fallbackScript('')
+            }
+        }
+        activeChoiceReactiveParam ('Job') {
+            description('Select job to build')
+            filterable()
+            choiceType('CHECKBOX')
+            groovyScript{
+                script ("list=[];(1..4).each {list.add(\"${job_prefix}-slave${it}-${job_suffix}\")} ")
+                fallbackScript('')
+            }
+        }
+    }
+}
