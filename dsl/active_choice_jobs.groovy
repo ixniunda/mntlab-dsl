69b61cecaa129efa3fe7eb3d3953ce18a93ad241
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-22 19:03:36 +0300

    Fixed dsl files

--- dsl/active_choice_jobs.groovy
+++ dsl/active_choice_jobs.groovy
@@ -2,8 +2,7 @@ import jenkins.model.*;
 import hudson.model.*
 def list =[]
 Jenkins.instance.getAllItems(AbstractProject.class).each {it ->
-  //if (it.fullName.matches('EPBYMINW2033\\/MNTLAB-ilakhtenkov-child(.+)')) {
-  if (it.fullName.matches('EPBYMINW2033\\/MNTLAB-ilakhtenkov-child(.+)")) {
+  if (it.fullName.matches('EPBYMINW2033\\/MNTLAB-ilakhtenkov-child(.+)')) {
 	list << "${it.name}:selected"
   }
 }
