d0e7636827164527f10ce3ca0115624500284537
Ihar Vauchok <napa93@mail.ru>
2017-11-22 15:41:26 +0000

    Feature/Added activeChoiceParams, conditionalSteps to main job

--- jobs.groovy
+++ jobs.groovy
@@ -1,18 +1,80 @@
 def student = 'ivauchok'
+def folder_name = 'Ihar Vauchok'
 def giturl = 'https://github.com/MNT-Lab/mntlab-dsl.git'
 
-folder(student) {
-    description("Folder containing all jobs for ${student}")
+folder(folder_name) {
+    description("Folder containing all jobs for ${folder_name}")
 }
 
-job("${student}/MNTLAB-${student}-main-build-job") {
+job("${folder_name}/MNTLAB-${student}-main-build-job") {
+    parameters {
+        activeChoiceParam('BRANCH_NAME') {
+            description('Allows user choose from multiple choices')
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('''import jenkins.model.*
+import hudson.model.*
+def command = "git ls-remote -h $gitURL"
+def proc = command.execute()
+proc.waitFor()              
+
+if ( proc.exitValue() != 0 ) {
+   println "Error, ${proc.err.text}"
+   System.exit(-1)
 }
 
+def branches = proc.in.text.readLines().collect { 
+    it.replaceAll(/[a-z0-9]*\trefs\\/heads\\//, '') 
+}
+return branches''')
+            }
+        }
+    }
+
+    parameters {
+        activeChoiceParam('BUILDS_TRIGGER') {
+            description('Allows user choose from multiple choices')
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('''import jenkins.model.*
+import hudson.model.*
+def list = []
+Jenkins.instance.getAllItems(AbstractProject.class).each {it ->
+  if(it.fullName.matches('Ihar Vauchok\\/.+')) {
+    list << "${it.name}:selected"
+  }
+}
+return list''')
+            }
+        }
+    }
+
+  steps {
+        conditionalSteps {
+          condition {
+            alwaysRun()
+          }
+            steps {
+        downstreamParameterized {
+            trigger('${BUILDS_TRIGGER}') {
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+                parameters {
+                    predefinedProp('BRANCH_NAME', '${BRANCH_NAME}')
+                }
+            }
+        }
+    }
+}
+  }
+}
 for(i in 1..4) {
-    job("${student}/MNTLAB-${student}-child${i}-build-job") {
+    job("${folder_name}/MNTLAB-${student}-child${i}-build-job") {
         scm {
             git(giturl)
         }
     }
 }
-
