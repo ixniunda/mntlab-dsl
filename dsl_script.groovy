e80d9d857b55b3f4eb049f746e5543b5b9fe2ea7
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-22 14:13:36 +0300

    Main jbo description added

--- dsl_script.groovy
+++ dsl_script.groovy
@@ -1,1 +1,14 @@
-//Initialization of branch
+def branchNane = "ilakhtenkov"
+
+job('MNTLAB-'+branchNane+'-main-build-job') {
+    description 'This is test main job'
+    scm {
+        git {
+          remote {
+            url('https://github.com/MNT-Lab/mntlab-dsl.git')
+          }
+          branch('$BRANCH_NAME')
+        }
+    }
+}
+       
