dfcc17760f5dbaeaeaad797f2abda165e5aa9296
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-22 14:33:40 +0300

    Added active choice plugin description

--- dsl_script.groovy
+++ dsl_script.groovy
@@ -2,6 +2,21 @@ def branchNane = "ilakhtenkov"
 
 job('MNTLAB-'+branchNane+'-main-build-job') {
     description 'This is test main job'
+    parameters {
+        activeChoiceParam('BRANCH_NAME') {
+            description('Allows branch multiple choices')
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('''def getbranches = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute()
+					return getbranches.text.readLines()
+        				.collect { it.split()[1].replaceAll('refs/heads/', '')  }
+        				.unique()
+        				.findAll { it.matches('ilakhtenkov|master') }''')
+                fallbackScript('return ["error"]')
+            }
+        }
+    }
     scm {
         git {
           remote {
