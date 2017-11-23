d3dd5c408c0e25b8f2fb506f178c86cfcbd42ace
Yury Karatseyeu <yury_karatseyeu@epam.com>
2017-11-23 13:00:47 +0000

    add choice

--- jobs.groovy
+++ jobs.groovy
@@ -3,4 +3,18 @@ def folderName = "EPBYMINW1374"
 def childJobs = 4
 
 
-job('/MNTLAB-'+branchName+'-main-build-job') {}
+job('/MNTLAB-'+branchName+'-main-t-build-job') {
+    parameters {
+        choiceParam('BRANCH_NAME', ['ykaratseyeu', 'master'], 'select branch name')     
+          activeChoiceReactiveParam('JOBS') {
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('["MNTLAB-ykaratseyeu-child1-build-job", "MNTLAB-ykaratseyeu-child2-build-job", "MNTLAB-ykaratseyeu-child3-build-job", "MNTLAB-ykaratseyeu-child4-build-job" ]')
+                
+            }
+            
+        }
+     
+      }
+         
+}
