4aaaa68997862cfbd8d776e5f0ba6bdd3c0f662f
Xetzer <xetzer@gmail.com>
2017-11-23 15:48:01 +0000

    added script to hsoose branch

--- jobs.groovy
+++ jobs.groovy
@@ -3,13 +3,15 @@ job('111111') {
      
 	parameters {
 		choiceParam('BRANCH_NAME', ['ilahutka', 'master'], 'select branch name')
-			activeChoiceReactiveParam('JOBS') {
-				choiceType('CHECKBOX')
-					groovyScript {
-						script('["job1", "job2"]')
-								}
-				}
+    			}
+	parameters {  
+        activeChoiceReactiveParam('JOBS') {
+			choiceType('CHECKBOX')
+				groovyScript {
+					script('["job1", "job2"]')
+							}
 			}
+		}
   
 steps {
 	shell('echo $BRANCH_NAME; echo $JOBS')
@@ -36,13 +38,42 @@ steps {
 			}
 		}
 }
-
+          
 job('job1') {
-	
-	parameters {
-		stringParam('BRANCH_NAME')
-				}
-	steps {
-		shell('echo $BRANCH_NAME')
-    }
+     
+       parameters {
+			stringParam('BRANCH_NAME')
+       }
+
+        parameters {
+			activeChoiceParam('BRANCH_NAME') {
+                description('Allows user choose from multiple choices')
+                choiceType('SINGLE_SELECT')
+           				groovyScript {
+                         	script('''
+def gitURL = "https://github.com/MNT-Lab/mntlab-dsl"
+def command = "git ls-remote -h $gitURL"
+
+def proc = command.execute()
+
+def branches = proc.in.text.readLines().collect {
+    it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
 }
+
+branches.each {println it}''')    
+							}
+				}
+        }
+
+  scm {
+            git {
+                remote {
+                    url('https://github.com/MNT-Lab/mntlab-dsl.git')
+                }
+                branch('*/$BRANCH_NAME')
+            }
+        }
+        steps {
+            shell ('echo $BRANCH_NAME' )
+        }
+    }
