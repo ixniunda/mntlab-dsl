217207af0552c726f435e7d2ee5cfd564dbfdb8f
Xetzer <xetzer@gmail.com>
2017-11-23 12:50:11 +0000

    first try

--- jobs.groovy
+++ jobs.groovy
@@ -1,1 +1,16 @@
 #!/home/jenkins/groovy-2.4.12/bin/groovy
+
+job('111111') {
+     
+      parameters {
+        choiceParam('BRANCH_NAME', ['ilahutka', 'master'], 'select branch name')
+        activeChoiceReactiveParam('JOBS') {
+           // filterable()
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('return ["job1", "job2"]')
+                //fallbackScript('"fallback choice"')
+            }
+        }
+    }
+ }
