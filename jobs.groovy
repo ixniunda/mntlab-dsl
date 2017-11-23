2d887502b942f6e8df4a6dcf607295bb36d0fd9c
Yury Karatseyeu <yury_karatseyeu@epam.com>
2017-11-23 14:45:06 +0000

    scm child

--- jobs.groovy
+++ jobs.groovy
@@ -3,20 +3,20 @@ def folderName = "EPBYMINW1374"
 def childJobs = 4
 
 
-job('MNTLAB-'+branchName+'-main-t-build-job') {
+job('MNTLAB-'+branchName+'-main-build-job') {
     parameters {
-        choiceParam('BRANCH_NAME', ['ykaratseyeu', 'master'], 'select branch name')     
-          activeChoiceReactiveParam('JOBS') {
+        choiceParam('BRANCH_NAME', ['ykaratseyeu', 'master'], 'select branch name')
+        activeChoiceReactiveParam('JOBS') {
             choiceType('CHECKBOX')
             groovyScript {
-                script('["MNTLAB-ykaratseyeu-child1-build-job1", "MNTLAB-ykaratseyeu-child2-build-job", "MNTLAB-ykaratseyeu-child3-build-job", "MNTLAB-ykaratseyeu-child4-build-job" ]')
-                
+                script('["MNTLAB-ykaratseyeu-child1-build-job", "MNTLAB-ykaratseyeu-child2-build-job", "MNTLAB-ykaratseyeu-child3-build-job", "MNTLAB-ykaratseyeu-child4-build-job" ]')
+
             }
-            
+
         }
-     
-      }
-  steps {
+
+    }
+    steps {
         conditionalSteps {
             condition {
                 shell('echo $JOBS | grep -q "child1"')
@@ -24,7 +24,7 @@ job('MNTLAB-'+branchName+'-main-t-build-job') {
             runner('Fail')
             steps {
                 downstreamParameterized {
-                    trigger("MNTLAB-ykaratseyeu-child1-build-job1") {
+                    trigger("MNTLAB-ykaratseyeu-child1-build-job") {
                         block {
                             buildStepFailure('FAILURE')
                             failure('FAILURE')
@@ -33,39 +33,116 @@ job('MNTLAB-'+branchName+'-main-t-build-job') {
                         parameters {
                             predefinedProp('BRANCH_NAME', '${BRANCH_NAME}')
                         }
+                    }
+                }
+            }
+        }
+        conditionalSteps {
+            condition {
+                shell('echo $JOBS | grep -q "child2"')
+            }
+            runner('Fail')
+            steps {
+                downstreamParameterized {
+                    trigger("MNTLAB-ykaratseyeu-child2-build-job") {
+                        block {
+                            buildStepFailure('FAILURE')
+                            failure('FAILURE')
+                            unstable('UNSTABLE')
+                        }
+                        parameters {
+                            predefinedProp('BRANCH_NAME', '${BRANCH_NAME}')
+                        }
+                    }
+                }
+            }
+        }
+        conditionalSteps {
+            condition {
+                shell('echo $JOBS | grep -q "child3"')
+            }
+            runner('Fail')
+            steps {
+                downstreamParameterized {
+                    trigger("MNTLAB-ykaratseyeu-child3-build-job") {
+                        block {
+                            buildStepFailure('FAILURE')
+                            failure('FAILURE')
+                            unstable('UNSTABLE')
+                        }
+                        parameters {
+                            predefinedProp('BRANCH_NAME', '${BRANCH_NAME}')
+                        }
+                    }
+                }
+            }
+        }
+        conditionalSteps {
+            condition {
+                shell('echo $JOBS | grep -q "child4"')
+            }
+            runner('Fail')
+            steps {
+                downstreamParameterized {
+                    trigger("MNTLAB-ykaratseyeu-child4-build-job") {
+                        block {
+                            buildStepFailure('FAILURE')
+                            failure('FAILURE')
+                            unstable('UNSTABLE')
+                        }
+                        parameters {
+                            predefinedProp('BRANCH_NAME', '${BRANCH_NAME}')
+                        }
+                    }
+                }
+            }
+        }
+
+    }
 }
+
+(1..4).each {
+
+
+    job('MNTLAB-'+branchName+'-child'+it+'-build-job') {
+
+
+        parameters {
+            stringParam('BRANCH_NAME')
+        }
+
+        parameters {
+            activeChoiceParam('BRANCH_NAME') {
+                description('Allows user choose from multiple choices')
+                choiceType('SINGLE_SELECT')
+                groovyScript {
+                    script('''
+	def remote_url="https://github.com/MNT-Lab/mntlab-dsl.git"
+	def command = [ "/bin/bash", "-c", "git ls-remote --heads " + remote_url + " | awk '{print \\$2}' | sort  -V | sed 's@refs/heads/@@'" ]
+	def process = command.execute()
+	def result = process.in.text.tokenize('\\n')
+	def branches = []
+	for(i in result) {
+        branches.add(i)
+	}
+	return branches''')
+                }
+            }
+        }
+
+        scm {
+
+            git {
+                remote {
+                    url('https://github.com/MNT-Lab/mntlab-dsl.git')
+                }
+                branch('*/${BRANCH_NAME}')
+            }
+
+            steps {
+                shell ('echo $JOB_NAME' )
             }
         }
     }
-         
-}
-}
-  
-  job('MNTLAB-ykaratseyeu-child1-build-job1') {
-  
-   parameters {
-            stringParam('BRANCH_NAME')
-}
-    
-    parameters {
-        activeChoiceParam('BRANCH_NAME') {
-            description('Allows user choose from multiple choices')
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script('''def remote_url="https://github.com/MNT-Lab/mntlab-dsl.git"
-def command = [ "/bin/bash", "-c", "git ls-remote --heads " + remote_url + " | awk '{print \\$2}' | sort  -V | sed 's@refs/heads/@@'" ]
-def process = command.execute()
 
-def result = process.in.text.tokenize('\\n')
-
-def branches = []
-for(i in result) {
-        branches.add(i)
 }
-return branches''')
-            }
-        }
-}
-  
-   
-  }
