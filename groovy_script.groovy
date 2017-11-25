2c00582e7da68c5c256c3ffe9d428618217d2835
AliaksandrTaran <ataran1996@gmail.com>
2017-11-25 15:16:24 +0000

    Change extension for file

---
+++ groovy_script.groovy
@@ -0,0 +1,151 @@
+tURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
+def command = "git ls-remote -h $gitURL"
+
+def proc = command.execute()
+proc.waitFor()
+
+if ( proc.exitValue() != 0 ) {
+    println "Error, ${proc.err.text}"
+    System.exit(-1)
+}
+
+def branches = proc.in.text.readLines().collect {
+    it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
+}
+
+
+
+
+job("EPBYMINW6405/MNTLAB-ataran-main-build-job") {
+    parameters {
+         choiceParam('BRANCH',['master','ataran']) 
+   	}
+  parameters{
+    activeChoiceParam('BUILD_JOBS'){
+      choiceType('CHECKBOX')
+      groovyScript {
+        script('''import jenkins.model.*;
+import hudson.model.*;
+def list=[];
+Jenkins.instance.getAllItems(AbstractProject.class).each {it ->if(it.name.matches('MNTLAB-ataran-child.+')){list<<"${it.name}:selected"};
+}
+return list ; 
+   		      ''')
+        fallbackScript('"Bad choice"');
+               }
+    }
+  }
+  steps {
+        conditionalSteps {
+          condition {
+            alwaysRun();
+          }
+           steps {
+              downstreamParameterized{
+              	trigger('$BUILD_JOBS') {
+                  block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+                  parameters{
+                    predefinedProp('BRANCH_NAME', '${BRANCH}')
+                  }               
+                }
+            }
+        }
+    }
+  }
+  
+}
+
+
+
+
+
+
+
+job("EPBYMINW6405/MNTLAB-ataran-child1-build-job") {
+  parameters {
+        stringParam('BRANCH_NAME')
+    }
+  parameters {
+         choiceParam('BRANCH_NAME',branches)
+        
+   	}
+  scm {
+        git{
+          remote{
+            url('https://github.com/MNT-Lab/mntlab-dsl.git')
+            branch('$BRANCH_NAME')
+          }
+        }
+    	
+    }
+  steps {
+        shell 'bash script.sh'
+    }
+}
+job("EPBYMINW6405/MNTLAB-ataran-child2-build-job") {
+  parameters {
+        stringParam('BRANCH_NAME')
+    }
+  parameters {
+         choiceParam('BRANCH_NAME',branches)
+        
+   	}
+  scm {
+        git{
+          remote{
+            url('https://github.com/MNT-Lab/mntlab-dsl.git')
+            branch('$BRANCH_NAME')
+          }
+        }
+    	
+    }
+  steps {
+        shell 'bash script.sh'
+    }
+}
+job("EPBYMINW6405/MNTLAB-ataran-child3-build-job") {
+  parameters {
+        stringParam('BRANCH_NAME')
+    }
+  parameters {
+         choiceParam('BRANCH_NAME',branches)
+        
+   	}
+  scm {
+        git{
+          remote{
+            url('https://github.com/MNT-Lab/mntlab-dsl.git')
+            branch('$BRANCH_NAME')
+          }
+        }
+    	
+    }
+  steps {
+        shell 'bash script.sh'
+    }
+}
+job("EPBYMINW6405/MNTLAB-ataran-child4-build-job") {
+  parameters {
+        stringParam('BRANCH_NAME')
+    }
+  parameters {
+         choiceParam('BRANCH_NAME',branches)
+        
+   	}
+  scm {
+        git{
+          remote{
+            url('https://github.com/MNT-Lab/mntlab-dsl.git')
+            branch('$BRANCH_NAME')
+          }
+        }
+    	
+    }
+  steps {
+        shell 'bash script.sh'
+    }
+}
